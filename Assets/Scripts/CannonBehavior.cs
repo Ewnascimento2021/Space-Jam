2c8b5670e9c2730651c14c5616817ed536ddf1b4
Heitor <heitor90276@gmail.com>
2022-07-09 20:57:13 -0300

    Wall Bounce Material and Cannon shooting

---
+++ Assets/Scripts/CannonBehavior.cs
@@ -0,0 +1,48 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CannonBehavior : MonoBehaviour
+{
+    [SerializeField]
+    private Transform aim;
+    [SerializeField]
+    private float timeInterval;
+
+    [SerializeField]
+    private Rigidbody2D projectile;
+    private float lastTimeStamp;
+
+    [SerializeField]
+    private float speed;
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Timer();
+    }
+    private void Timer()
+
+    {
+        if (Time.time - lastTimeStamp >= timeInterval)
+        {
+            lastTimeStamp = Time.time;
+            Fire();
+
+        }
+    }
+
+    private void Fire()
+    {
+        Rigidbody2D newProjectile = Instantiate(projectile, aim.position, aim.rotation);
+        newProjectile.AddForce(transform.up * speed);
+    }
+}
+
+
+
